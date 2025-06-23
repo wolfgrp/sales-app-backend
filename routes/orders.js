43fdbfc84e3e00a4c98098b2128c5f1d4412b354
wolfgrp <shreechitraguptaenterprises@gmail.com>
2025-06-23 13:19:32 +0530

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,1 +1,9 @@
+// 📁 backend/routes/orders.js
+const express = require('express');
+const router = express.Router();
+const { submitOrder, getAllOrders } = require('../controllers/orderController');
 
+router.post('/submit', submitOrder);
+router.get('/all', getAllOrders);
+
+module.exports = router;
