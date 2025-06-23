d3f89fff35a298f508f1e6f902c810754457e4b3
wolfgrp <shreechitraguptaenterprises@gmail.com>
2025-06-23 13:51:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');
 
 dotenv.config();
 const app = express();
